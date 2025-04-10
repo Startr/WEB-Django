0abf2b01e868b0e8d9836ddd15bf4e9701a9ef5a
OpenCo <admin@openco.ca>
2025-04-10 11:28:36 +0000

    feat: Add django-constance package with database extras to Pipfile and update lock file

--- our_site/our_site/settings.py
+++ our_site/our_site/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'constance',
+    'constance.backends.database',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'constance.context_processors.config',  # Add constance context processor
             ],
         },
     },
@@ -143,3 +146,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = '/'
+
+# Constance settings
+CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
+
+CONSTANCE_CONFIG = {
+    'SITE_FAVICON': ('media/logos/Study_Logo_H_Blue-Crest.png', 'Site favicon path from media directory', str),
+}
+
+CONSTANCE_CONFIG_FIELDSETS = {
+    'Site Settings': (
+        'SITE_FAVICON',
+    ),
+}
